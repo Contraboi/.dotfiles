6009941bbe621716a70e5522c063c0fe99b3f59e
Selmir Nedzibi <Contraboi@users.noreply.github.com>
2024-04-18 16:49:29 +0200

    refactor(nvim): delete packer

--- nvim/after/plugin/telescope.lua
+++
@@ -1,9 +0,0 @@
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>pf', "<cmd>lua require'telescope.builtin'.find_files({ find_command = {'rg', '--ignore', '--files', '--hidden', '-g', '!.git' }})<cr>", {})
-vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-vim.keymap.set('n', '<leader>ps', function()
-	builtin.live_grep({})
-end)
-
-vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
-
--- nvim/lua/contraboi/init.lua
+++ nvim/lua/contraboi/init.lua
@@ -1,33 +1,16 @@
-require("contraboi.set")
-require("contraboi.remap")
+require 'contraboi.remap'
+require 'contraboi.init_lazy'
 
-local augroup = vim.api.nvim_create_augroup
-local ContraGroup = augroup('ContraGroup', {})
+-- [[ Basic Autocommands ]]
+--  See `:help lua-guide-autocommands`
 
-local autocmd = vim.api.nvim_create_autocmd
-local yank_group = augroup('HighlightYank', {})
-
-function R(name)
-    require("plenary.reload").reload_module(name)
-end
-
-autocmd('TextYankPost', {
-    group = yank_group,
-    pattern = '*',
-    callback = function()
-        vim.highlight.on_yank({
-            higroup = 'IncSearch',
-            timeout = 40,
-        })
-    end,
+-- Highlight when yanking (copying) text
+--  Try it with `yap` in normal mode
+--  See `:help vim.highlight.on_yank()`
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
 })
-
-autocmd({"BufWritePre"}, {
-    group = ContraGroup,
-    pattern = "*",
-    command = [[%s/\s\+$//e]],
-})
-
-vim.g.netrw_browse_split = 0
-vim.g.netrw_banner = 0
-vim.g.netrw_winsize = 25
